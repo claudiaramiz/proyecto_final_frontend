f2e39b81da22334e489ef384034afbf3d119a836
Claudia Ramirez <mely.ramiz07@gmail.com>
2022-08-13 10:17:51 -0500

    Feat: add transaltion in menu

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,9 +4,11 @@ import { NavLink } from 'react-router-dom';
 import { LoginButton } from './Login';
 import { LogoutButton } from './Logout';
 import { useAuth0 } from '@auth0/auth0-react';
+import { useTranslation } from 'react-i18next';
 
 const Navigation = () => {
     const {isAuthenticated} = useAuth0();
+    const {t} = useTranslation();
 
     return (
 
@@ -17,12 +19,12 @@ const Navigation = () => {
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link as={NavLink} to="/">Home</Nav.Link>
-                    <Nav.Link as={NavLink} to="/BlogList">Blogs</Nav.Link>
+                    <Nav.Link as={NavLink} to="/">{t("home")}</Nav.Link>
+                    <Nav.Link as={NavLink} to="/BlogList">{t("blogs")}</Nav.Link>
                     {isAuthenticated ? (
                         <>
-                        <Nav.Link as={NavLink} to="/Categories">Categories</Nav.Link>
-                        <Nav.Link as={NavLink} to="/Blog">Nuevo Blog</Nav.Link>
+                        <Nav.Link as={NavLink} to="/Categories">{t("categories")}</Nav.Link>
+                        <Nav.Link as={NavLink} to="/Blog">{t("newBlog")}</Nav.Link>
                             </>
                          ) : (
                             <></>
