c499cd5168ac9f66d9e8e389c220ce978208d2ab
CSE3008 <211368@utags.edu.mx>
2022-08-13 02:27:55 -0500

    feat: Login funtions

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,30 @@
-import { Link } from 'react-router-dom';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+
+import { Container, Row, Col} from 'react-bootstrap';
+import {LoginButton} from './Login'
+import { LogoutButton } from './Logout';
+import {Profile} from './Profile'
+import { useAuth0 } from '@auth0/auth0-react';
 
 function Home() {
+    const {isAuthenticated} = useAuth0();
     return (
         <Container>
             <Row className="mt-5">
                 <Col xs={{ span: 12 }} md={{ span: 6 }} className="mb-5">
                     <h2>Bienvenid@ a Oh My Blog!</h2>
                     <p>Publica todo lo que deseas compartir!</p>
-                    <p>
-                        <Button as={Link} to="/login" className="ml-1">Ingresa</Button> </p>
+                 
+                    <p></p>   
+                    {isAuthenticated ? (
+                        <>
+                        <Profile />
+                        <p></p>
+                        <LogoutButton />
+                            </>
+                         ) : (
+                            <LoginButton />
+                     )}
+                        
                 </Col>
                 <Col>
                     <img
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,109 +1,18 @@
-import React, { useState } from "react";
-import { Container, Row, Col, Alert } from 'react-bootstrap';
+import React from "react";
+import { useAuth0 } from '@auth0/auth0-react';
+import { Button, Nav} from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
-function Login() {
-  // React States
-  const [errorMessages, setErrorMessages] = useState({});
-  const [isSubmitted, setIsSubmitted] = useState(false);
+export const LoginButton = () =>{
+  const {loginWithRedirect } = useAuth0();
 
+  return <Button variant="primary" onClick={() => loginWithRedirect()}> Login </Button>
 
-  // User Login info
-  const database = [
-    {
-      "id": 1,
-      "lastname": "Ramirez",
-      "firstname": "Claudia",
-      "username": "claudiar",
-      "password": "12345"
-    },
-    {
-      "id": 2,
-      "lastname": "Soto",
-      "firstname": "Carolina",
-      "username": "carosoto",
-      "password": "54321"
-    },
-    {
-      "id": 3,
-      "lastname": "Estrada",
-      "firstname": "Caro",
-      "username": "estrada",
-      "password": "12367"
-    }
-  ];
+};
+export const LoginNav = () =>{
+  const {loginWithRedirect } = useAuth0();
 
-  const errors = {
-    uname: "Invalid username",
-    pass: "Invalid password"
-  };
+  return  <Nav.Link as={NavLink} to={loginWithRedirect}>Login</Nav.Link>
+  
+};
 
-  const handleSubmit = (event) => {
-    //Prevent page reload
-    event.preventDefault();
-
-    var { uname, pass } = document.forms[0];
-
-    // Find user login info
-    const userData = database.find((user) => user.username === uname.value);
-
-    // Compare user info
-    if (userData) {
-      if (userData.password !== pass.value) {
-        // Invalid password
-        setErrorMessages({ name: "pass", message: errors.pass });
-      } else {
-        setIsSubmitted(true);
-      }
-    } else {
-      // Username not found
-      setErrorMessages({ name: "uname", message: errors.uname });
-    }
-  };
-
-  // Generate JSX code for error message
-  const renderErrorMessage = (name) =>
-    name === errorMessages.name && (
-      <div className="error">{errorMessages.message}</div>
-    );
-
-  // JSX code for login form
-  const renderForm = (
-    <Container>
-      <Row className="mt-5">
-        <Col xs={{ span: 12 }} md={{ span: 6 }} className="mb-5">
-          <div className="form-outline mb-6">
-            <form onSubmit={handleSubmit}>
-              <div className="input-container">
-                <label className="form-label">Username </label>
-                <input type="text" name="uname" className="form-control" required />
-                {renderErrorMessage("uname")}
-              </div>
-              <div className="input-container">
-                <label className="form-label">Password </label>
-                <input type="password" name="pass" className="form-control" required />
-                {renderErrorMessage("pass")}
-              </div>
-              <div className="input-container">
-                <div className="input-container">&nbsp;</div>
-                <input type="submit" className="btn btn-primary btn-block" />
-              </div>
-            </form>
-          </div>
-        </Col>
-      </Row>
-    </Container>
-
-  );
-
-  return (
-    <div className="app">
-      <div className="login-form">
-        <Alert.Heading>Sign In</Alert.Heading>
-        <div className="title"></div>
-        {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
-      </div>
-    </div>
-  );
-}
-
-export default Login;
